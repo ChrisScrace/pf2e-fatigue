85335df7d3e8fc9584ba6f01700d53771c325fed
Chris Scrace <chrisscrace@gmail.com>
2023-04-13 08:10:38 +0100

    check for forge days rest feat

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 1.2.0
+* when a player has the Forde Day's Rest feat they will be asked if their rest was 12 hours and if so will adjust the timer accordingly
+* Duration added on for Forge Day's Rest is configurable in settings, defaults to 4 hours as per the rules
+
+# 1.1.3
+* Release
--- scripts/fatigue.js
+++ scripts/fatigue.js
@@ -1,4 +1,5 @@
 const FATIGUED_EFFECT_ID = 'Compendium.pf2e.conditionitems.HL2l2VRSaQHu9lUw';
+const FORGE_DAYS_REST_FEAT_ID = 'Compendium.pf2e.feats-srd.ZxiAMposVPDNPwxI';
 const SECONDS_IN_HOUR = 3600;
 
 class Fatigue {
@@ -6,11 +7,17 @@ class Fatigue {
 
     static FLAGS = {
         START_TIME: 'startTime',
-        IS_FATIGUED: 'isFatigued'
+        IS_FATIGUED: 'isFatigued',
+        HAS_RESTED_TWELVE_HOURS: "hasRestedTwelveHours"
     }
 
     static SETTINGS = {
-        FATIGUE_DURATION: 'fatigue-duration'
+        FATIGUE_DURATION: 'fatigue-duration',
+        FORGE_DAYS_REST_DURATION: 'forge-days-rest-duration'
+    }
+
+    static TEMPLATES = {
+        FATIGUE: `modules/${this.ID}/templates/rest-for-twelve.hbs`
     }
 
     static log(force, ...args) {
@@ -30,6 +37,15 @@ class Fatigue {
             config: true,
             hint: `PF2E-FATIGUE.settings.${this.SETTINGS.FATIGUE_DURATION}.Hint`,
         });
+
+        game.settings.register(this.ID, this.SETTINGS.FORGE_DAYS_REST_DURATION, {
+            name: `PF2E-FATIGUE.settings.${this.SETTINGS.FORGE_DAYS_REST_DURATION}.Name`,
+            default: 4,
+            type: Number,
+            scope: 'world',
+            config: true,
+            hint: `PF2E-FATIGUE.settings.${this.SETTINGS.FORGE_DAYS_REST_DURATION}.Hint`,
+        });
     }
 
     static ensureAllTimersSet() {
@@ -42,9 +58,19 @@ class Fatigue {
     }
 
     static checkTimer(worldTime) {
-        const fatigueDelay = game.settings.get(Fatigue.ID, Fatigue.SETTINGS.FATIGUE_DURATION);
+        const waitTimeInHours = game.settings.get(Fatigue.ID, Fatigue.SETTINGS.FATIGUE_DURATION);
+
         game.actors.filter((actor) => actor.type === "character").forEach(character => {
-            if (worldTime >= FatigueData.getTracker(character) + (fatigueDelay * SECONDS_IN_HOUR)
+
+            let adjustedWaitTimeInHours = waitTimeInHours;
+            //add more time when Forge Day's Rest applies
+            if (true === FatigueData.getForgeDaysRested(character)) {
+                this.log(false, character.name + " has FDR");
+                adjustedWaitTimeInHours += game.settings.get(Fatigue.ID, Fatigue.SETTINGS.FORGE_DAYS_REST_DURATION);
+            }
+            const waitTimeInSeconds = adjustedWaitTimeInHours * SECONDS_IN_HOUR;
+
+            if (worldTime >= FatigueData.getTracker(character) + (waitTimeInSeconds)
                 && true != FatigueData.getFatigued(character)) {
                 FatigueData.setFatigued(character, true);
                 this.log(false, character.name + " has gained fatigued!");
@@ -58,8 +84,19 @@ class Fatigue {
         this.log(false, character.name + ' rested for the night at ' + worldTime);
         FatigueData.setTracker(character, worldTime);
         FatigueData.setFatigued(character, false);
+        if (this.hasItem(character, FORGE_DAYS_REST_FEAT_ID)) {
+            this.log(false, character.name + " has Forge Day's Rest!");
+            new ForgeDaysRestForm(character).render(true);
+        };
     }
 
+    static hasItem(character, itemID) {
+        return this.getItemFromActor(character, itemID) != null;;
+    }
+
+    static getItemFromActor(actor, sourceId) {
+        return actor.items.find(item => item.getFlag("core", "sourceId") === sourceId);
+    }
     static async addEffect(character) {
         const effect = (await fromUuid(FATIGUED_EFFECT_ID)).toObject();
         effect.flags.core.sourceId = FATIGUED_EFFECT_ID;
@@ -83,6 +120,73 @@ class FatigueData {
     static getFatigued(character) {
         return character.getFlag(Fatigue.ID, Fatigue.FLAGS.IS_FATIGUED);
     }
+
+    static setForgeDaysRested(character, hasForgeDaysRested) {
+        return character.setFlag(Fatigue.ID, Fatigue.FLAGS.HAS_RESTED_TWELVE_HOURS, hasForgeDaysRested);
+    }
+
+    static getForgeDaysRested(character) {
+        return character.getFlag(Fatigue.ID, Fatigue.FLAGS.HAS_RESTED_TWELVE_HOURS);
+    }
+}
+
+class ForgeDaysRestForm extends FormApplication {
+    constructor(character) {
+        super();
+        this.character = character;
+      }
+
+    static get defaultOptions() {
+        const defaults = super.defaultOptions;
+      
+        const overrides = {
+          height: 'auto',
+          width: 'auto',
+          id: 'rested-for-twelve',
+          template: Fatigue.TEMPLATES.FATIGUE,
+          title: 'Forge Day\'s Rest',
+          submitOnChange: true,
+          closeOnSubmit: true
+        };
+      
+        const mergedOptions = foundry.utils.mergeObject(defaults, overrides);
+        
+        return mergedOptions;
+      }
+
+      activateListeners(html) {
+        super.activateListeners(html);
+        html.on('click', "[data-action]", this._handleButtonClick.bind(this));
+      }
+
+      async _handleButtonClick(event) {
+        const clickedElement = $(event.currentTarget);
+        const action = clickedElement.data().action;
+    
+        switch (action) {
+          case 'yes': {
+            Fatigue.log(false, this.character.name + " slept for 12 hours");
+            FatigueData.setForgeDaysRested(this.character, true);
+            break;
+          }
+    
+          case 'no': {
+            Fatigue.log(false, this.character.name +" did not sleep for 12 hours");
+            FatigueData.setForgeDaysRested(this.character, false);
+            break;
+          }
+    
+          default:
+            ToDoList.log(false, 'Invalid action detected', action);
+        }
+        this.close();
+      }
+
+      getData(options) {
+        return {
+          character: options.character
+        }
+      }
 }
 
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => { registerPackageDebugFlag(Fatigue.ID); });
